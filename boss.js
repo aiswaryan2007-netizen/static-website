a337ba9f784155345182d5b000a19c1d00bbc492
Aiswaryan <aiswaryan2007@gmail.com>
2026-07-14 12:17:27 +0530

    first message

---
+++ boss.js
@@ -0,0 +1,38 @@
+// var a=5;
+// var b=10;
+// console.log(a+b)
+// let a=9;
+// let b=8;
+// console.log(a-b)
+// var a=6;
+// var b=9;
+// console.log(a*b)
+// let a=20;
+// let b=85;
+// console.log(a/b)
+// const a=98;
+// const b=90;
+// console.log(a<b)
+// const a=40;
+// const b=50;
+// console.log(a>b)
+// var a=76;
+// var b=59;
+// console.log(a==b)
+// console.log(a<=b)
+// console.log(a>=b)
+// var a=10;
+// var b=10;
+// console.log(a===b)
+// function addfunction(a,b){
+//     return a+b
+// }
+// console.log(addfunction(30,50))
+// function square(a){
+//     return a*a
+// }
+// console.log(square(5))
+// const square = n => n*n;
+// console.log(square(9))
+const square = (a,b) => a+b
+console.log(square(10,60))
